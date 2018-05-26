1406f19c0fe386e72e345dd65178598921ce5d05
Vagif Zeynalov <vagif@zeynalov.com>
2018-05-25 23:49:59 -0700

    Version 0.2.1 * Update to Java 8u171 * Update to Java cookbook 2.1.0

--- attributes/java.rb
+++ attributes/java.rb
@@ -3,5 +3,5 @@ default['java']['install_flavor'] = 'oracle'
 default['java']['oracle']['accept_oracle_download_terms'] = true
 default['java']['set_default'] = true
 
-default['java']['jdk']['8']['x86_64']['url'] = 'https://s3-us-west-2.amazonaws.com/vagifzeynalov-chef/jdk-8u151-linux-x64.tar.gz'
-default['java']['jdk']['8']['x86_64']['checksum'] = 'c78200ce409367b296ec39be4427f020e2c585470c4eed01021feada576f027f'
+default['java']['jdk']['8']['x86_64']['url'] = 'https://s3-us-west-2.amazonaws.com/vagifzeynalov-chef/jdk-8u171-linux-x64.tar.gz'
+default['java']['jdk']['8']['x86_64']['checksum'] = 'b6dd2837efaaec4109b36cfbb94a774db100029f98b0d78be68c27bec0275982'
--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'vagif@zeynalov.com'
 license 'All Rights Reserved'
 description 'Installs/Configures oracle_java'
 long_description 'Installs/Configures oracle_java'
-version '0.1.1'
+version '0.2.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 source_url 'https://github.com/vagifzeynalov/chef_cookbook_oracle_java'
 
-depends 'java', '= 1.50.0'
+depends 'java', '= 2.1.0'
